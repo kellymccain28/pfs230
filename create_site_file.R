413b1621b25385c07c249c480f186393d32f9efa
Kelly McCain <kem22@dide.local>
2026-07-07 16:02:10 +0100

    add plot_site_files to helper_functions.R

--- create_site_file.R
+++ create_site_file.R
@@ -1,7 +1,5 @@
 ## Script to run site files
 
-
-
 # pak::pak("mrc-ide/site")
 # pak::pak('mrc-ide/netz')
 # devtools::install_github('mrc-ide/malariasimulation@Pfs230_2026')
@@ -12,14 +10,8 @@ library(site)
 library(cowplot)
 library(netz)
 
-
-
-
-
 year <- 365
 source('helper_functions.R')
-
-
 
 # List of sites to use
 # data.frame with country, name of admin1 unit
@@ -30,8 +22,6 @@ site_list <- data.frame(country_code = countries,
                         admin_1_name = admin1s,
                         ur = 'rural')
 
-
-
 #### Fetch site files for all sites: to be run once
 # produces saved site files
 site_files <- purrr::pmap(site_list, function(country_code, admin_1_name, ur){
@@ -40,51 +30,68 @@ site_files <- purrr::pmap(site_list, function(country_code, admin_1_name, ur){
                   ur = ur)
 })
 
-
-
 names(site_files) <- paste0(site_list$country_code, '_', site_list$admin_1_name, '_', site_list$ur)
 saveRDS(site_files, 'site_files/all_site_files.rds')
 
 
-
 # Then run gather_params for the whole list of sites above
 # which should extract important bits of ach of the site files and add in ages,etc.
-
-
-
-params <- gather_params(site_files$TZA_Pwani_rural,
+model_input <- gather_params(site_files$TZA_Pwani_rural,
                         hum_pop = 20000,
                         quick_run = TRUE,
                         parameter_draw = 0)
 
-
-
-all_params <- lapply(site_files,
+all_model_input <- lapply(site_files,
                      gather_params,
                      hum_pop = 20000,
                      quick_run = TRUE)
-
+names(all_model_input) <- paste0(site_list$country_code, '_', site_list$admin_1_name, '_', site_list$ur)
+saveRDS(all_model_input, 'site_files/all_model_input.rds')
 
 
 # Calibration of the model for each site
 
 
-
-
-
 # output of above used as input into a run_model function
-output <- run_model(model_input = params,
+output <- run_model(model_input = model_input,
                     verbose = TRUE)
 saveRDS(output, 'testoutput.rds')
 
+all_model_output <- lapply(all_model_input,
+                           run_model)
+names(all_model_output) <- paste0(site_list$country_code, '_', site_list$admin_1_name, '_', site_list$ur)
+saveRDS(all_model_output, 'all_model_output.rds')
 
 
 # process the model output -- i.e. with postie as in infectivity work from the other day
-output_processed <- process_output(output)
+site_list$site_key <- paste0(site_list$country_code, '_', site_list$admin_1_name, '_', site_list$ur)
+all_processed_output <- list()
+for (s in site_list$site_key){
+  message(s)
+  output_processed <- process_output(all_model_output[[s]],
+                                     all_model_input[[s]])
+
+  all_processed_output[[s]] <- output_processed
+
+}
+names(all_processed_output) <- paste0(site_list$country_code, '_', site_list$admin_1_name, '_', site_list$ur)
+saveRDS(all_processed_output, 'all_processed_output.rds')
 
 
 
 # plot the interventions over time using postie
+plot_site_files(model = output,
+                model_input = model_input,
+                site_file = site_files$TZA_Pwani_rural)
+
+for (s in site_list$site_key){
+  message(s)
+
+  plot_site_files(model = all_model_output[[s]],
+                  model_input = all_model_input[[s]],
+                  site_file = site_files[[s]])
+
+}
 
 
 
--- helper_functions.R
+++ helper_functions.R
@@ -51,12 +51,143 @@ fetch_all_sites <- function(country_code,
 }
 
 
-# Diagnostics for site files - to do
+# Diagnostics for site files
 # function to look at the interventions over time
-diagnostics_site_files <- function(site){
+# Function to plot interventions over time
 
-  # Plot interventions over time
+#' @param site_file site file for 1 site
+#' @param model raw model output
+#' @param model_input run parameters and parameter list
+plot_site_files <- function(model,
+                            model_input,
+                            site_file){
+
+  params <- model_input$param_list
+  key <- paste0(site_file$sites$country, '_', site_file$sites$name_1, '_', site_file$sites$urban_rural)
+
+  #equal spacing around colour wheel
+  gg_color_hue <- function(n) {
+    hues = seq(15, 375, length = n + 1)
+    hcl(h = hues, l = 65, c = 100)[1:n]
+  }
+
+  #Treatment coverage / drugs used
+  dr <- data.frame('t'= 1985+(params$clinical_treatment_timesteps[[1]]-1)/365,
+                   'non_act' =  params$clinical_treatment_coverages[[1]],
+                   'act' = params$clinical_treatment_coverages[[2]])
+  drm <- reshape2::melt(dr, id.vars = 't')
+
+  n = 2
+  cols = gg_color_hue(n)
+  pl1 <- ggplot(drm) +
+    geom_col(aes(x = t, y = value, fill = variable),
+             position = position_stack(reverse = TRUE)) +
+    theme_classic() + xlim(c(1999.5,NA)) +
+    labs(y = 'Treatment Coverage',
+         x = 'Year',
+         title = key) +
+    ylim(c(0,1)) + theme(legend.position = c(0.2,0.78)) +
+    scale_fill_manual(values = cols, labels = c('Non-ACT','ACT'),
+                      name = 'Treatment type')
+  pl1
+
+  params$bednet_coverages
+  params$bednet_rn
+
+  #if you've run the model
+  dfi <- data.frame('yr' = 1985 + model$timestep/365,
+                    'itn_use' = model$n_use_net/model$n_age_0_36500[9000])
+  tst <- data.frame('yr' = site_file$interventions$itn$use$year + 0.5,
+                    'itn_use' = site_file$interventions$itn$use$itn_use,
+                    'itn_type' = site_file$interventions$itn$implementation$net_type)
+  pl2 <- ggplot(dfi) + geom_line(aes(x = yr, y = itn_use)) +
+    geom_point(data = tst, aes(x = yr, y = itn_use, color = itn_type)) +
+    theme_classic() +
+    labs(y = 'ITN use in the population',
+         x = 'Year',
+         title = key) +
+    scale_color_manual(values = cols, labels = c('Pyrethroid only','Pyrethroid-PBO'),
+                       name = 'ITN type distributed') +
+    xlim(c(1999.5,NA)) + theme(legend.position = c(0.15,0.7))#theme(legend.position = 'top')
+  pl2
+
+  ##Pyrethroid resistance
+  dfr <- data.frame('yr' = site_file$vectors$pyrethroid_resistance$year + 0.5,
+                    'res' = site_file$vectors$pyrethroid_resistance$pyrethroid_resistance)
+  pl3 <- ggplot(dfr) + geom_point(aes(x = yr, y = res), color = 'purple') +
+    theme_classic() +
+    xlab('Year') + ylab('Degree of pyrethroid resistance') + ylim(c(0,1))
+  pl3
+
+  #SMC
+
+  dfs <- data.frame('yr' = site_file$interventions$smc$implementation$year + 0.5,
+                    'smc_cov' = site_file$interventions$smc$implementation$smc_cov,
+                    'smc_rounds' = site_file$interventions$smc$implementation %>% ungroup() %>%
+                      group_by(year) %>% mutate(n_rounds = max(round)) %>% pull(n_rounds))
+  pl4 <- ggplot(dfs) + geom_point(aes(x = yr, y = smc_cov, color = factor(smc_rounds))) +
+    theme_classic() +
+    labs(y = 'SMC coverage',
+         x = 'Year',
+         title = key) +
+    theme(legend.position = c(0.2,0.78)) + ylim(c(0,1)) +
+    scale_color_manual(values = c('slateblue'),
+                       name = 'No. of\nrounds\nper year')
+  pl4
+
+  #species & seasonality
+  dsp <- data.frame('yr' = 1985 + model$timestep/365,
+                    'fun'= model$total_M_funestus/model$n_age_0_36500[9000],
+                    'gamb' = model$total_M_gambiae/model$n_age_0_36500[9000],
+                    'arab' = model$total_M_arabiensis/model$n_age_0_36500[9000],
+                    'inc' = 18*model$n_inc_clinical_1825_5840,
+                    'totM' = (model$total_M_funestus+model$total_M_gambiae+model$total_M_arabiensis)/model$n_age_0_36500[9000])
+  dspm <- reshape2::melt(dsp, id.vars = 'yr')
+  ggplot(dspm) + geom_line(aes(x = yr, y = value, color = variable)) +
+    theme_classic() + xlim(c(2010,2012))
+
+  seas <- function(model){
+    mn <- min(model$timestep)
+    mx <- max(model$timestep)
+    store <- rep(0,365)
+    for(i in mn:mx){
+      sr <- model$timestep[i] %% 365
+      store[sr] <- store[sr] + model$n_inc_clinical_0_36500[i]/model$n_age_0_36500[i]
+    }
+    bray <- cumsum(store)
+    return(bray/bray[365])
+  }
+
+  fg <- seas(model = model)
+  dfg <- data.frame('t' = seq(1,365,1), fg = fg)
+  pl5 <- ggplot(dfg) + geom_hline(yintercept = 100*c(0.1,0.9), color = 'red', alpha = .3) +
+    geom_line(aes(x=t,y=100*fg)) + theme_classic() +
+    scale_x_continuous(breaks = c(0,91,182,274),
+                       labels = c('Jan','April','July','Oct')) +
+    labs(y = 'Averaged cumulative incidence (%)',
+         title = paste0(key, ': Seasonality of malaria incidence'),
+         x = 'Month')
+  pl5
+
+  # plaux <- cowplot::plot_grid(pl1,pl3,pl4,pl5, nrow = 2)
+  # cowplot::plot_grid(pl2,plaux, nrow = 2, rel_heights = c(0.6,1))
+  # ggsave('pfVIMT_site_files/BE.pdf', height = 8.7, width = 9)
+
+  # Open the PDF device and specify the file path and name
+  pdf(file = paste0("site_files/site_file_plots/", key, '.pdf'))
+
+
+  # Generate plots
+  print(pl1)
+  print(pl2)
+  print(pl3)
+  print(pl4)
+  print(pl5)
+
+  # Close the PDF device to finalize the file
+  dev.off()
 }
+
 
 # Function to determine the age groups and time horizons
 pull_age_groups_time_horizon<- function(quick_run = T){
@@ -75,7 +206,7 @@ pull_age_groups_time_horizon<- function(quick_run = T){
   } else{
 
     pop_val<- 50000
-    term_yr<- 226
+    term_yr<- 2026
 
     min_ages = c(0, 2, 5, 16, 0)*year
     max_ages = c(5, 10, 16, 100, 100) * year
@@ -153,6 +284,7 @@ gather_params <- function(site, # this will be the output of the fetch_all_sites
     'pop_val' = run_params$pop_val,
     'burnin' =  run_params$burnin
   )
+
   return(inputs)
 }
 
@@ -169,8 +301,32 @@ gather_params <- function(site, # this will be the output of the fetch_all_sites
 #   tapply(pfpr, year, mean)
 # }
 
-# Calibration of the model to site-specific information -- to do later
+# Calibration of the model to site-specific information -- to check later
+pr_match <- function(x, y){
 
+  data <- baseline_parameters[x,]
+  params <- unlist(data$params, recursive = FALSE)
+  params$timesteps <- data$sim_length + data$warmup
+
+  # defining target as pfpr value in last 2 years of simulation
+  target <- rep(data$pfpr, 2)
+
+  set.seed(1234)
+  out <- cali::calibrate(parameters = params,
+                         target = target,
+                         summary_function = annual_pfpr_summary,
+                         tolerance = 0.001,
+                         low = 0.1,
+                         high = 1500)
+
+  # store init_EIR results as an .rds file to be read in later
+  PR <- data.frame(scenarioID = x,  drawID = y)
+  PR$starting_EIR <- out
+  PR$ID <- data$ID
+
+  print(paste0('Finished scenario ',x))
+  saveRDS(PR, paste0('PrEIR/PRmatch_draws_', data$ID, '.rds'))
+}
 
 
 # Function to run model for the individual
@@ -220,10 +376,10 @@ run_model<- function(model_input,
 
 # Processing the model output from run_model()
 # outputs a processed data frame
-process_output <- function(model){
+process_output <- function(model, model_input){
 
   # Drop burnin
-  raw_output <- postie::drop_burnin(model, burnin = run_parameters$burnin)
+  raw_output <- postie::drop_burnin(model, burnin = model_input$burnin)
 
   message('calculating rates')
 
@@ -231,12 +387,12 @@ process_output <- function(model){
     raw_output,
     baseline_year = 1
   ) %>%# add identifying information to output
-    mutate(scen_name = scen_name_val,
-           pfpr = pfpr_val,
-           seas_name = seas_name_val,
-           parameter_draw = run_parameters$parameter_draw,
-           population = run_parameters$population,
-           burnin = run_parameters$burnin)
+    mutate(country = model_input$country,
+           ur = model_input$ur,
+           site_name = model_input$site_name,
+           parameter_draw = model_input$parameter_draw,
+           population = model_input$population,
+           burnin = model_input$burnin)
 
   rates_annual <- rates %>%
     dplyr::summarise(
@@ -250,12 +406,12 @@ process_output <- function(model){
       time = mean(time),
       .by = c(year, age_lower, age_upper)
     ) %>%# add identifying information to output
-    mutate(scen_name = scen_name_val,
-           pfpr = pfpr_val,
-           seas_name = seas_name_val,
-           parameter_draw = run_parameters$parameter_draw,
-           population = run_parameters$population,
-           burnin = run_parameters$burnin)
+    mutate(country = model_input$country,
+           ur = model_input$ur,
+           site_name = model_input$site_name,
+           parameter_draw = model_input$parameter_draw,
+           population = model_input$population,
+           burnin = model_input$burnin)
 
   message('calculating prevalence')
   # Get prevalence
@@ -283,22 +439,41 @@ process_output <- function(model){
            starts_with('n_age')) %>%
     mutate(year = ceiling(timestep / 365),
            time = timestep) %>%
-    mutate(n_age_0_36500 = 10000,
-           prop_under5 = n_age_0_1825 / n_age_0_36500,
-           prop_SAC = n_age_1826_5840 / n_age_0_36500,
-           prop_16plus = n_age_5841_36500 / n_age_0_36500) %>%
+    mutate(prop_under5 = n_age_0_1825 / n_age_0_36500,
+           prop_SAC = n_age_1825_5840 / n_age_0_36500,
+           prop_16plus = n_age_5840_36500 / n_age_0_36500) %>%
     rowwise() %>%
-    mutate(infectivity_total = infectivity_under5 + infectivity_SAC + infectivity_16plus) %>%
+    # get total summed infectivity
+    mutate(infectivity_sum_total = infectivity_under5 + infectivity_SAC + infectivity_16plus) %>%
     ungroup() %>%
-    mutate(prop_inf_under5 = infectivity_under5 / infectivity_total,
-           prop_inf_SAC = infectivity_SAC / infectivity_total,
-           prop_inf_16plus = infectivity_16plus / infectivity_total) %>%# add identifying information to output
-    mutate(scen_name = scen_name_val,
-           pfpr = pfpr_val,
-           seas_name = seas_name_val,
-           parameter_draw = run_parameters$parameter_draw,
-           population = run_parameters$population,
-           burnin = run_parameters$burnin)
+
+    # Proportion of summed infectivity of total summed infectivity
+    mutate(prop_sum_inf_under5 = infectivity_under5 / infectivity_sum_total,
+           prop_sum_inf_SAC = infectivity_SAC / infectivity_sum_total,
+           prop_sum_inf_16plus = infectivity_16plus / infectivity_sum_total) %>%
+
+    # get mean per-person infectivity for each age group
+    mutate(mean_inf_under5 = infectivity_under5 / n_age_0_1825,
+           mean_inf_SAC = infectivity_SAC / n_age_1825_5840,
+           mean_inf_16plus = infectivity_16plus / n_age_5840_36500) %>%
+
+    rowwise() %>%
+    # sum of means above should be approx equal to the originaly infectvity output
+    mutate(sum_mean_infectivity = mean_inf_under5 + mean_inf_SAC + mean_inf_16plus) %>%
+    ungroup() %>%
+
+    # get proportion of mean infectivity by age group
+    mutate(prop_mean_inf_under5 = mean_inf_under5 / sum_mean_infectivity,
+           prop_mean_inf_SAC = mean_inf_SAC / sum_mean_infectivity,
+           prop_mean_inf_16plus = mean_inf_16plus / sum_mean_infectivity) %>%
+
+    # add identifying information to output
+    mutate(country = model_input$country,
+           ur = model_input$ur,
+           site_name = model_input$site_name,
+           parameter_draw = model_input$parameter_draw,
+           population = model_input$population,
+           burnin = model_input$burnin)
 
   infectivity_annual <- infectivity %>%
     dplyr::summarise(
@@ -306,19 +481,23 @@ process_output <- function(model){
       time = mean(.data$time),
       .by = c('year')
     ) %>% select(-time, -timestep)%>%# add identifying information to output
-    mutate(scen_name = scen_name_val,
-           pfpr = pfpr_val,
-           seas_name = seas_name_val,
-           parameter_draw = run_parameters$parameter_draw,
-           population = run_parameters$population,
-           burnin = run_parameters$burnin)
+    mutate(country = model_input$country,
+           ur = model_input$ur,
+           site_name = model_input$site_name,
+           parameter_draw = model_input$parameter_draw,
+           population = model_input$population,
+           burnin = model_input$burnin)
 
-  return(list('raw_output' = raw_output,
-              'infectivity' = infectivity,
-              'infectivity_annual' = infectivity_annual,
-              'daily_output' = daily_output,
-              'annual_output' = annual_output,
-              "parameters" = params_scenario))
+  processed_out <- list('raw_output' = raw_output,
+                        'infectivity' = infectivity,
+                        'infectivity_annual' = infectivity_annual,
+                        'daily_epi_output' = daily_output,
+                        'annual_epi_output' = annual_output,
+                        "model_input" = model_input)
+
+  saveRDS(processed_out, paste0('outputs/processed_out', paste0(model_input$country, '_', model_input$site_name, '_', model_input$ur), '.rds'))
+
+  return(processed_out)
 }
 
 
